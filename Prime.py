4d7bc4155dd0b45b36717fe222f72948ec0c2e11
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-17 04:05:12 +0100

    solidifying knowledge

--- Prime.py
+++ Prime.py
@@ -11,3 +11,18 @@ def is_prime(digit):
     return True
 
 print(is_prime(21))
+
+def prime(number):
+  if number < 2:
+    return False
+  elif number == 2:
+    return False
+  else:
+    for x in range(2, number):
+      print(x)
+      if number % x == 0:
+        return False
+    else:
+      return True
+
+print(prime(19))
