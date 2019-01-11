dd97775d2035ac2e7345908a7f696cddbe28ce3f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-11 22:22:58 +0100

    solidifying knowledge

--- primeNum.py
+++ primeNum.py
@@ -11,3 +11,17 @@ def prime(number):
     return True
 
 print(prime(19))
+
+
+def checkIfNumberIsPrime(number):
+  if number == 2:
+    return True
+  if number < 2:
+    return False
+  for x in range(2, number - 1):
+    if number % x == 0:
+      return False
+      print(number + ' is not a prime number.')
+      break
+  else:
+    return True
